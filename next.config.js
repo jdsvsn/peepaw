df3a55c0c1535c62da0fdb00e99df3d6bb85a3bc
jdsvsn <judeparu123@gmail.com>
2026-03-17 16:22:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-/** @type {import('next').NextConfig} */ 
-const nextConfig = { output: 'export', images: { unoptimized: true } } 
+/** @type {import('next').NextConfig} */
+const nextConfig = { 
+  output: 'export', 
+  images: { unoptimized: true },
+  basePath: '/peepaw',
+  assetPrefix: '/peepaw'
+}
 module.exports = nextConfig
